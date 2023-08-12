71b16d8f19d1ec1ddb0eb1109237235b385e9ffd
Arthur Machado Wiebusch <arthur.wieb@gmail.com>
2023-08-11 21:16:25 -0700

    baseurl fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333",
-  // baseURL: "https://foodexplorer-backend-v6hy.onrender.com",
+  // baseURL: "http://localhost:3333",
+  baseURL: "https://foodexplorer-backend-v6hy.onrender.com",
 });
 
 api
